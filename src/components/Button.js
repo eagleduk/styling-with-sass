df4b514f0854bbdd1ef2bae3da8b78f47f78fd73
eagleduk <eagleduk@gmail.com>
2020-10-26 21:51:52 +0900

    sass 설정, 다양한 사이즈의 class 설정하기

---
+++ src/components/Button.js
@@ -0,0 +1,18 @@
+import React from "react";
+import classNames from "classnames";
+import "./Button.scss"
+
+// size: large, medium, small
+function Button({ children, size }) {
+    return (
+        <button className={classNames("Button", size)}>
+            {children}
+        </button>
+    )
+}
+
+Button.defaultProps = {
+    size: 'medium'
+};
+
+export default Button;
